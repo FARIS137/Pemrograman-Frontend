18b9d5605ab66ec9a0b60cdd9a2cf5935c475c38
riss aja <buatyt542@gma.com>
2023-03-19 17:28:01 +0700

    Pertemuan 4

---
+++ pertemuan-4/movie-database/src/components/Footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+       <footer>
+           <h2>Copyright @FARIS137</h2>
+           <p>Created By Reach.js</p>
+       </footer>
+    );
+   }
+
+export default Footer;
---
+++ pertemuan-4/movie-database/src/components/Hello.js
@@ -0,0 +1,12 @@
+function Hello(props){
+    const { name } = props;
+
+    return (
+        <div>
+            <h2>Hello React</h2>
+            <p>saya {name} - Frontend Engineer</p>
+        </div>
+    );
+}
+
+export default Hello;
