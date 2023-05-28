d1571b9396778218fdd8fececa2ca984f4dfdba9
Muhammad Faris <96936492+FARIS137@users.noreply.github.com>
2023-05-28 18:35:54 +0700

    Update Indonesia.js

--- frontend-programming-final-project/react-covid/src/components/Indonesia/Indonesia.js
+++ frontend-programming-final-project/react-covid/src/components/Indonesia/Indonesia.js
@@ -10,22 +10,16 @@ function Indonesia() {
         <h2 className={styles.indonesia__title}>Indonesia</h2>
         <p className={styles.indonesia__description}>Data Covid Berdasarkan Indonesia</p>
         <div className={styles.cardContainer}>
-          <div className={styles.card}>
-            <h2 className={styles.cardTitle}>Confirmed</h2>
-            <p className={styles.cardDescription1}>{indonesia[0].total}</p>
-          </div>
-          <div className={styles.card}>
-            <h2 className={styles.cardTitle}>Recorvered</h2>
-            <p className={styles.cardDescription2}>{indonesia[1].total}</p>
-          </div>
-          <div className={styles.card}>
-            <h2 className={styles.cardTitle}>Death</h2>
-            <p className={styles.cardDescription3}>{indonesia[2].total}</p>
-          </div>
+          {indonesia.map((data, index) => (
+            <div key={index} className={styles.card}>
+              <h2 className={styles.cardTitle}>{data.status}</h2>
+              <p className={styles.cardDescription}>{data.total}</p>
+            </div>
+          ))}
         </div>
       </section>
     </div>
   );
-};
+  }
 
 export default Indonesia;
