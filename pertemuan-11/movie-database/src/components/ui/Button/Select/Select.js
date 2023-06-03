94e3c5f885b1e0b311ac202c5c50d1e61c278547
riss aja <buatyt542@gma.com>
2023-06-03 16:29:39 +0700

    idk

---
+++ pertemuan-11/movie-database/src/components/ui/Button/Select/Select.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const select = styled.select`
+outline: none;
+padding: 0.5rem 0.5rem;
+border-radius: 10px;
+background-color: #fff;
+`;
+
+export default select;
---
+++ pertemuan-11/movie-database/src/pages/Movie/Popular.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { useEffect, useState } from "react";
+import Movies from "../../components/Movies/Movies";
+
+function PopularMovie() {
+  // eslint-disable-next-line no-unused-vars
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const URL = `https://api.themoviedb.org/3/movie/popular?api_key=f1ac2165ff93884cbed80a7f73613552`;
+
+  //Membuat State Movies
+  const [movies, setMovies] = useState([]);
+  
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(async () => {
+    const response = await axios(URL);
+    setMovies(response.data.results);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  });
+
+console.log(movies);
+  return (
+    <>
+      <Movies movies={movies} />
+    </>
+  );
+}
+
+export default PopularMovie;
