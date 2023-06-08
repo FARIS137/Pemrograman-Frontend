10a2b12fdae0e37336cc8d83ad3784165937a810
Muhammad Faris <96936492+FARIS137@users.noreply.github.com>
2023-06-08 14:29:08 +0700

    Add files via upload

---
+++ movie-database/src/pages/Movie/Popular.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { useEffect, useState } from "react";
+import Movies from "../../components/Movies/Movies";
+
+function PopularMovie() {
+  // eslint-disable-next-line no-unused-vars
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`;
+
+  //Membuat State Movies
+  const [movies, setMovies] = useState([]);
+  
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  async function fetchMovie() {
+    const response = await axios(URL);
+    setMovies(response.data.results);
+  }
+
+useEffect(() => {
+  fetchMovie();
+// eslint-disable-next-line react-hooks/exhaustive-deps
+}, []);
+
+
+console.log(movies);
+  return (
+    <>
+      <Movies movies={movies} setMovies={setMovies} />
+    </>
+  );
+  }
+
+export default PopularMovie;
