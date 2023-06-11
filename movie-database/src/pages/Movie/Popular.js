cb45458a819dbc388239ce05bf86ac93ea1a8dee
Muhammad Faris <96936492+FARIS137@users.noreply.github.com>
2023-06-11 17:01:13 +0700

    Update Popular.js

--- movie-database/src/pages/Movie/Popular.js
+++ movie-database/src/pages/Movie/Popular.js
@@ -1,33 +1,28 @@
-import axios from 'axios';
+import axios from "axios";
 import { useEffect, useState } from "react";
+import Hero from "../../components/Hero/Hero";
 import Movies from "../../components/Movies/Movies";
+import ENDPOINTS from "../../utils/constants/endpoints";
 
 function PopularMovie() {
-  // eslint-disable-next-line no-unused-vars
-  const API_KEY = process.env.REACT_APP_API_KEY;
-  const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`;
-
   //Membuat State Movies
   const [movies, setMovies] = useState([]);
-  
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  async function fetchMovie() {
-    const response = await axios(URL);
+
+  async function getpopularMovies() {
+    const response = await axios(ENDPOINTS.POPULAR);
     setMovies(response.data.results);
   }
 
-useEffect(() => {
-  fetchMovie();
-// eslint-disable-next-line react-hooks/exhaustive-deps
-}, []);
+  useEffect(() => {
+    getpopularMovies();
+  }, []);
 
-
-console.log(movies);
   return (
     <>
-      <Movies movies={movies} setMovies={setMovies} />
+      <Hero />
+      <Movies movies={movies} title="Popular" />
     </>
   );
-  }
+}
 
 export default PopularMovie;
