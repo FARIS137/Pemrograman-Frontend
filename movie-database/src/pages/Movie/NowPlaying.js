5c2a9f7ff8b69445b72774cbba6af37849d88768
Muhammad Faris <96936492+FARIS137@users.noreply.github.com>
2023-06-11 17:00:18 +0700

    Update NowPlaying.js

--- movie-database/src/pages/Movie/NowPlaying.js
+++ movie-database/src/pages/Movie/NowPlaying.js
@@ -1,33 +1,27 @@
-import axios from 'axios';
+import axios from "axios";
 import { useEffect, useState } from "react";
 import Movies from "../../components/Movies/Movies";
+import Hero from "../../components/Hero/Hero";
+import ENDPOINTS from "../../utils/constants/endpoints";
 
 function NowPlayingMovie() {
-  // eslint-disable-next-line no-unused-vars
-  const API_KEY = process.env.REACT_APP_API_KEY;
-  const URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}`;
-
-  //Membuat State Movies
   const [movies, setMovies] = useState([]);
-  
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+
   async function fetchMovie() {
-    const response = await axios(URL);
+    const response = await axios(ENDPOINTS.NOWPLAYING);
     setMovies(response.data.results);
   }
 
-useEffect(() => {
-  fetchMovie();
-// eslint-disable-next-line react-hooks/exhaustive-deps
-}, []);
+  useEffect(() => {
+    fetchMovie();
+  }, []);
 
-
-console.log(movies);
   return (
     <>
-      <Movies movies={movies} setMovies={setMovies} />
+      <Hero />
+      <Movies movies={movies} title="Now Playing" />
     </>
   );
-  }
+}
 
 export default NowPlayingMovie;
