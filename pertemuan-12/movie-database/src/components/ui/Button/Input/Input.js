bf271f3883d6c90c593e53913ae62234bbbb60fd
riss aja <buatyt542@gma.com>
2023-06-11 17:06:12 +0700

    pertemuan 12

---
+++ pertemuan-12/movie-database/src/components/ui/Button/Input/Input.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+outline: none;
+  padding: 0.5rem ;
+  border-radius: 10px;
+  border: 1px solid #64748b;
+`;
+
+export default Input;
