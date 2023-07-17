50969211d6c2eab7074a3c1b6f1e0b50a00d1151
Muhammad Faris <96936492+FARIS137@users.noreply.github.com>
2023-07-17 13:36:02 +0700

    Add files via upload

---
+++ Final-Project-UAS/react-covid/src/utils/constants/endpoint.js
@@ -0,0 +1,7 @@
+const BASE_URL = `https://covid-fe-2023.vercel.app/api`;
+
+const endpoints = {
+  Global:`${BASE_URL}/global.json`,
+  Indonesia: `${BASE_URL}/indonesia.json`,
+};
+export default endpoints;
