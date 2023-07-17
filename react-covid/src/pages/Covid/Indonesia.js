e7b881e3fb1c46998c74d42a361d6ce5120070b2
Muhammad Faris <96936492+FARIS137@users.noreply.github.com>
2023-07-17 13:09:12 +0700

    Update Indonesia.js

--- react-covid/src/pages/Covid/Indonesia.js
+++ react-covid/src/pages/Covid/Indonesia.js
@@ -1,13 +1,26 @@
 import Hero from "../../components/Hero/Hero";
 import Indonesian from "../../components/Indonesian/Indonesian";
 import Regions from "../../components/RegionsProv/Regions";
+import React, { useEffect, useState } from "react";
 
 function Indonesiacovid() {
+  const [regions, setRegions] = useState([]);
+  const [indonesian, setIndonesian] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://covid-fe-2023.vercel.app/api/indonesia.json`)
+      .then((response) => response.json())
+      .then((data) => {
+        setRegions(data.regions);
+        setIndonesian(data.indonesia);
+      });
+  }, []);
+
   return (
     <>
       <Hero />
-      <Indonesian />
-      <Regions />
+      <Indonesian indonesian={indonesian} />
+      <Regions regions={regions} />
     </>
   );
 }
