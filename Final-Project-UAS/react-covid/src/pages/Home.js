392f6495ebffe313ffc3aab58d63e4ffb8d7e339
Muhammad Faris <96936492+FARIS137@users.noreply.github.com>
2023-07-17 13:41:15 +0700

    Update Home.js

--- Final-Project-UAS/react-covid/src/pages/Home.js
+++ Final-Project-UAS/react-covid/src/pages/Home.js
@@ -1,13 +1,28 @@
 import Global from "../components/Globals/Global";
 import Regions from "../components/RegionsGlob/Regions";
-import Hero from "./../components/Hero/Hero";
+import Hero from "../components/Hero/Hero";
+import React, { useEffect, useState } from "react";
+import ENDPOINTS from "../utils/constants/endpoint";
+
 
 function Home() {
+  const [regions, setRegions] = useState([]);
+  const [global, setGlobal] = useState([]);
+
+  useEffect(() => {
+    fetch(ENDPOINTS.Global)
+      .then((response) => response.json())
+      .then((data) => {
+        setRegions(data.regions);
+        setGlobal(data.global);
+      });
+  }, []);
+
   return (
     <>
       <Hero />
-      <Global />
-      <Regions />
+      <Global global={global} />
+      <Regions regions={regions} />
     </>
   );
 }
