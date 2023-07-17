57f0cc8abdc72330eb6a3b812a888b68b0f823f1
Muhammad Faris <96936492+FARIS137@users.noreply.github.com>
2023-07-17 13:41:47 +0700

    Update Indonesia.js

--- Final-Project-UAS/react-covid/src/pages/Covid/Indonesia.js
+++ Final-Project-UAS/react-covid/src/pages/Covid/Indonesia.js
@@ -1,13 +1,28 @@
 import Hero from "../../components/Hero/Hero";
 import Indonesian from "../../components/Indonesian/Indonesian";
 import Regions from "../../components/RegionsProv/Regions";
+import React, { useEffect, useState } from "react";
+import ENDPOINTS from "../../utils/constants/endpoint";
+
 
 function Indonesiacovid() {
+  const [regions, setRegions] = useState([]);
+  const [indonesian, setIndonesian] = useState([]);
+
+  useEffect(() => {
+    fetch(ENDPOINTS.Indonesia)
+      .then((response) => response.json())
+      .then((data) => {
+        setRegions(data.regions);
+        setIndonesian(data.indonesia);
+      });
+  }, []);
+
   return (
     <>
       <Hero />
-      <Indonesian />
-      <Regions />
+      <Indonesian indonesian={indonesian} />
+      <Regions regions={regions} />
     </>
   );
 }
