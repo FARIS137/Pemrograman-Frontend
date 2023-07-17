9fee365291a1dce0e05a3c9c7c170aac907a702d
Muhammad Faris <96936492+FARIS137@users.noreply.github.com>
2023-07-17 13:09:40 +0700

    Update Home.js

--- react-covid/src/pages/Home.js
+++ react-covid/src/pages/Home.js
@@ -1,13 +1,26 @@
 import Global from "../components/Globals/Global";
 import Regions from "../components/RegionsGlob/Regions";
-import Hero from "./../components/Hero/Hero";
+import Hero from "../components/Hero/Hero";
+import React, { useEffect, useState } from "react";
 
 function Home() {
+  const [regions, setRegions] = useState([]);
+  const [global, setGlobal] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://covid-fe-2023.vercel.app/api/global.json`)
+      .then((response) => response.json())
+      .then((data) => {
+        setRegions(data.regions);
+        setGlobal(data.global);
+      });
+  }, []);
+
   return (
     <>
       <Hero />
-      <Global />
-      <Regions />
+      <Global global={global} />
+      <Regions regions={regions} />
     </>
   );
 }
