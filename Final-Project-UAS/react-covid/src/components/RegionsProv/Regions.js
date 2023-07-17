51772c811f29b6d00df7cb3b157f1ce42711ba91
Muhammad Faris <96936492+FARIS137@users.noreply.github.com>
2023-07-17 13:39:45 +0700

    Update Regions.js

--- Final-Project-UAS/react-covid/src/components/RegionsProv/Regions.js
+++ Final-Project-UAS/react-covid/src/components/RegionsProv/Regions.js
@@ -1,50 +1,38 @@
-import React, { useEffect, useState } from "react";
 import StyledRegionsprov from "./Regions.styled.js";
 import Region from "../RegionProv/Region";
 
-function Regions() {
-  const [regions, setRegions] = useState([]);
-
-  useEffect(() => {
-    fetch(`https://covid-fe-2023.vercel.app/api/indonesia.json`)
-      .then((response) => response.json())
-      .then((data) => {
-        setRegions(data.regions);
-      });
-  }, []);
+function Regions(props) {
+  const { regions } = props;
 
   return (
     <StyledRegionsprov>
-    <div>
-      <section>
-        <h2 >Situation by Provinces</h2>
-        <p>
-          Data Covid Berdasarkan Provinsi
-        </p>
-        <div className="Group">
-          <table>
-          <thead>
-            <tr>
-              <th>No</th>
-              <th>Provinsi</th>
-              <th>Positif</th>    
-              <th>Sembuh</th>
-              <th>Meninggal</th>
-              <th>Dirawat</th>
-            </tr>
-          </thead>
-          <tbody>
-            {regions.map((region, index) => (
-              <Region key={index} region={region} index={index}/>
-            ))}
-          </tbody>
-        </table>
-        </div>
-      </section>
-    </div>
+      <div>
+        <section>
+          <h2>Situation by Provinces</h2>
+          <p>Data Covid Berdasarkan Provinsi</p>
+          <div className="Group">
+            <table>
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Provinsi</th>
+                  <th>Positif</th>
+                  <th>Sembuh</th>
+                  <th>Meninggal</th>
+                  <th>Dirawat</th>
+                </tr>
+              </thead>
+              <tbody>
+                {regions.map((region, index) => (
+                  <Region key={index} region={region} index={index} />
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </section>
+      </div>
     </StyledRegionsprov>
   );
 }
 
 export default Regions;
-
